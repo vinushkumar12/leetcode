47c5a6b61089d995daa5504aa7ac2e6a2269b8ac
vinushkumar12 <89699440+vinushkumar12@users.noreply.github.com>
2023-06-07 10:36:05 -0400

    Time: 71 ms (25.84%) | Memory: 18 MB (16.87%) - LeetSync

---
+++ valid-palindrome/valid-palindrome.py
@@ -0,0 +1,10 @@
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.sub(r'[\W_]+', '', s)
+        s = s.lower()
+        print(s)
+        for i in range(len(s)):
+            if (s[i] != s[len(s) - (i + 1)]):
+                return False
+        return True
