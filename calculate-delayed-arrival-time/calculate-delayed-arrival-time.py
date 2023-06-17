e953fc16faa6bb296a1807bfefc0384595b53aae
vinushkumar12 <89699440+vinushkumar12@users.noreply.github.com>
2023-06-17 10:15:19 -0400

    Time: 57 ms (7.27%) | Memory: 16.2 MB (35.17%) - LeetSync

--- calculate-delayed-arrival-time/calculate-delayed-arrival-time.py
+++ calculate-delayed-arrival-time/calculate-delayed-arrival-time.py
@@ -1,8 +1,3 @@
 class Solution:
     def findDelayedArrivalTime(self, arrivalTime: int, delayedTime: int) -> int:
-        answer = arrivalTime + delayedTime
-        if (answer < 24):
-            return answer
-        elif (answer == 24):
-            return 0
-        return answer % 24
+        return (arrivalTime + delayedTime) % 24
