050bf300cc980535e4040175cdfbac9b4c5cfa32
vinushkumar12 <89699440+vinushkumar12@users.noreply.github.com>
2023-06-23 11:58:20 -0400

    Time: 49 ms (60.27%) | Memory: 16.3 MB (69.03%) - LeetSync

--- sort-colors/sort-colors.py
+++ sort-colors/sort-colors.py
@@ -1,15 +1,14 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         def quicksort(nums):
-            if len(nums) <= 1:
+            if (len(nums) <= 1):
                 return nums
             else:
                 pivot = nums[0]
                 less = [x for x in nums[1:] if x <= pivot]
-                greater = [x for x in nums[1:] if x > pivot]
-                return quicksort(less) + [pivot] + quicksort(greater)     
-        modified = quicksort(nums)
-        for i in range(len(modified)):
-            nums[i] = modified[i]
+                more = [x for x in nums[1:] if x > pivot]
+                return quicksort(less) + [pivot] + quicksort(more)
+        ans = quicksort(nums)
+        for i in range(len(ans)):
+            nums[i] = ans[i]
         return nums
-        
