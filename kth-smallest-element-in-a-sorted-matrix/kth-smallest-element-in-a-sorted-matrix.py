d0ee00342293f3f82cb6671936f72e2b9d68c166
vinushkumar12 <89699440+vinushkumar12@users.noreply.github.com>
2023-06-23 11:45:25 -0400

    Time: 310 ms (5.08%) | Memory: 37.1 MB (5.26%) - LeetSync

--- kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -1,7 +1,25 @@
 import numpy as np
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        answer = []
-        for i in matrix:
-            answer += i
-        return sorted(answer)[k-1]
+        n = len(matrix)
+        # Set the range of the search to be between the smallest element
+        # and the largest element in the matrix
+        left, right = matrix[0][0], matrix[n-1][n-1]
+        
+        # Binary search for the kth smallest element
+        while left < right:
+            mid = (left + right) // 2
+            count = 0
+            j = n - 1
+            # Count the number of elements in the matrix that are less than or equal to mid
+            for i in range(n):
+                while j >= 0 and matrix[i][j] > mid:
+                    j -= 1
+                count += j + 1
+            # Update the range of the search
+            if count < k:
+                left = mid + 1
+            else:
+                right = mid
+        
+        return left
