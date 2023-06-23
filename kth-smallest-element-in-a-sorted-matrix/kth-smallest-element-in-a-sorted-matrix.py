ea1ddbfc4842996614fb18b8d0eb7e26aecbd138
vinushkumar12 <89699440+vinushkumar12@users.noreply.github.com>
2023-06-23 11:43:44 -0400

    Time: 273 ms (7.52%) | Memory: 38.2 MB (5.26%) - LeetSync

--- kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -1,9 +1,7 @@
 import numpy as np
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        list1 = []
-        for i in range(len(matrix)):
-            for j in range(len(matrix)):
-                list1.append(matrix[i][j])
-        list1.sort()
-        return list1[k-1]
+        answer = []
+        for i in matrix:
+            answer += i
+        return sorted(answer)[k-1]
