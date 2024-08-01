2f5db210239550612bb55f81c04282e6d11792f8
vinushkumar12 <89699440+vinushkumar12@users.noreply.github.com>
2024-08-01 13:27:32 -0400

    Time: 486 ms (92.65%) | Memory: 31.1 MB (11.12%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -3,8 +3,6 @@ class Solution:
         high_sum = nums[0]
         curr_sum = 0
         for i in nums:
-            print("curr", curr_sum)
-            print("high", high_sum)
             curr_sum += i
             if curr_sum > high_sum:
                 high_sum = curr_sum
